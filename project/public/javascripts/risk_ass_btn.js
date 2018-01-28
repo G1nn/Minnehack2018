ddb57e27d847d15746217f2f0963496b86c5d814
shaneong <ongxx103@umn.edu>
2018-01-28 11:59:12 -0600

    calendar

--- project/public/javascripts/risk_ass_btn.js
+++ project/public/javascripts/risk_ass_btn.js
@@ -3,10 +3,43 @@ var user_name = "John"; // hard coded value
 function getRiskAss(name) {
   return 0.6;
 }
+
+// function move() {
+//     var elem = document.getElementById("myBar");
+//     var width = 1;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//         if (width >= 60) {
+//             clearInterval(id);
+//         } else {
+//             width++;
+//             elem.style.width = width + '%';
+//         }
+//     }
+//}
+
+function move(risk_per) {
+  // risk_per : A NUMBER BETWEEN 0 and 100
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= risk_per) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+            elem.innerHTML = width * 1 + '%';
+        }
+    }
+}
+
+
 $('#rsk_ass_btn').on('click',function() {
 
-  $('#user_risk_as').text("Hello " + user_name + " your current risk assment is: " + getRiskAss(user_name));
+  $('#user_risk_as').text(user_name + "'s risk assessment is...");
+  move(getRiskAss() * 100);
 
-  
+
 
 });
