6fb7410d88ab110a7b4ce61b05d3ac61f4d1fcda
shaneong <ongxx103@umn.edu>
2018-01-28 09:48:03 -0600

    yes

--- project/public/javascripts/creative.js
+++ project/public/javascripts/creative.js
@@ -1,8 +1,103 @@
-(function($) {
+$(document).ready(function() {
+
+  // page is now ready, initialize the calendar...
+
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    nowIndicator: true,
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    eventLimit: true, // allow "more" link when too many events
+    selectable: true,
+    selectHelper: true,
+    select: function(start, end) {
+      var title = prompt('Event Title:');
+      var eventData;
+      if (title) {
+        eventData = {
+          title: title,
+          start: start,
+          end: end
+        };
+        $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+      }
+      $('#calendar').fullCalendar('unselect');
+    },
+    events: [
+      {
+        title: 'All Day Event',
+        start: '2018-01-01'
+      },
+      {
+        title: 'Long Event',
+        start: '2018-01-07',
+        end: '2018-01-10',
+        color: 'red'
+
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2018-01-09T16:00:00',
+        color: 'red'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2018-01-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2018-01-11',
+        end: '2018-01-13',
+        color: 'red'
+
+      },
+      {
+        title: 'Meeting',
+        start: '2017-12-12T10:30:00',
+        end: '2017-12-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2017-12-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2017-12-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: '2017-12-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: '2017-12-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2017-12-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2017-12-28'
+      }
+    ]
+  })
+
+});
+
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    console.log('i am called')
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +111,8 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
+    console.log('i am called')
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,7 +123,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
